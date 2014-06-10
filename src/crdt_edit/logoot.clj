a034c1a276febf7d6f074795c9a488f58f9776df
Jason Gilman <jason@element84.com>
2014-06-09 20:54:13 -0400

    Implemented functions to manipulate document.

--- src/crdt_edit/logoot.clj
+++ src/crdt_edit/logoot.clj
@@ -1,5 +1,6 @@
 (ns crdt-edit.logoot
-  "An implementation of the Logoot CRDT.")
+  "An implementation of the Logoot CRDT."
+  (:require [clojure.string :as str]))
 
 (defn- lexi-compare
   "Implements lexigraphical comparison between two vectors. Compares each item from the left side
@@ -10,6 +11,13 @@
       ;; nil means that they were equal for the sizes they had. Compare their counts in that case
       (compare (count v1) (count v2))))
 
+(defprotocol ToLogootString
+  "TODO"
+  (to-logoot-string
+    [i]
+    "TODO"))
+
+;; TODO document this
 (defrecord PositionIdentifier
   [
    ;; Integer identifying a position
@@ -17,7 +25,9 @@
    ;; A symbol identifying the site
    site
    ]
+  
   java.lang.Comparable
+  
   (compareTo 
     [pid1 pid2]
     (let [{pos1 :pos site1 :site} pid1
@@ -26,33 +36,78 @@
         (< pos1 pos2) -1
         (> pos1 pos2) 1
         :else 
-        (compare site1 site2)))))
+        (compare site1 site2))))
+  
+  ToLogootString
+  
+  (to-logoot-string
+    [pid]
+    (str "<" (:pos pid) "," (:site pid) ">")))
 
+;; TODO document this
+(defrecord Position
+  [
+   ;; A list of position identifiers
+   identifiers
+   ]
+  java.lang.Comparable
+  
+  (compareTo 
+    [pos1 pos2]
+    (let [ids1 (:identifiers pos1)
+          ids2 (:identifiers pos2)]
+      (lexi-compare ids1 ids2)))
+  
+  ToLogootString
+  
+  (to-logoot-string
+    [pc]
+    (str/join "." (map to-logoot-string identifiers))))
+
+;; TODO document this
 (defrecord PositionedCharacter
   [
    ;; A single character with a position in the document.
    character
    
-   ;; Sequence of idenfiers indicating the position within the document
-   identifiers
+   ;; The position of the character within the document
+   position
    ]
   
   java.lang.Comparable
+  
   (compareTo 
     [pc1 pc2]
-    (let [ids1 (:identifiers pc1)
-          ids2 (:identifiers pc2)]
-      (lexi-compare ids1 ids2))))
+    (compare (:position pc1) (:position pc2)))
+  
+  ToLogootString
+  
+  (to-logoot-string
+    [pc]
+    (str "(" 
+         (to-logoot-string position))
+         ","
+         character
+         ")"))
 
 (defn pos-id
   "Creates a new position idenfier"
   [pos site]
   (->PositionIdentifier pos site))
 
+(defn position
+  "Creates a new position at the same site"
+  [site & ids]
+  (->Position (mapv #(pos-id % site) ids)))
+
+(defn pos-char
+  "Creates a new positioned character"
+  [c site & ids]
+  (->PositionedCharacter
+    c
+    (apply position site ids)))
+
 (comment 
-  
-  (compare [1 2 3] [55])
-  
   
   (lexi-compare [1 2 3] [1 2]) ; = 1 meaning 123 comes after 12
   (lexi-compare [1 2] [1 2 3]) ; = -1 meaning 1 2 comes before 1 2 3
@@ -74,7 +129,6 @@
         0 "p1 is equal to p2"
         1 "p1 is greater than p2")))
   
-  
   (test-compare 
     (->PositionedCharacter 
       \a
@@ -91,23 +145,60 @@
        ; (pos-id 3 :a)
        ]))
   
-  (compare (pos-id 0 :a) (pos-id 0 :a))
-  (compare (pos-id 0 :a) (pos-id 0 :b))
-  (compare (pos-id 0 :b) (pos-id 0 :a))
-  (compare (pos-id 0 :b) (pos-id 1 :b))
-  (compare (pos-id 1 :b) (pos-id 0 :b))
+  (-> (create)
+      (insert (pos-char \a :site 0))
+      (insert (pos-char \b :site 1))
+      (insert (pos-char \c :site 2))
+      (insert (pos-char \d :site 3))
+      
+      ;; between b and c
+      (insert (pos-char \q :site 1 5))
+      
+      ;; between q and c
+      (insert (pos-char \r :site 1 6))
+      
+      ;; a concurrent edit from another site
+      ;; placed after :site
+      (insert (pos-char \t :siteb 1 6))
+      
+      ;; Now we delete the r 
+      ;; Q: Should site matter? Because in our implementation it does.
+      (delete (position :site 1 6))
+      
+      doc-string
+      ; logoot-string
+      println
+      )
   
   
   );; comment end
 
 
+;; TODO performance note: If writing a function that applies many updates to a document use transients
+
+;; TODO we may want to refactor this and use a sorted-map instead of a sorted set. The keys would be
+;; positions and the values would be characters. 
+
 (defn create
   "Returns a new empty data structure"
   []
-  )
+  (sorted-set))
+
+(defn doc-string
+  "Returns the document as a string"
+  [document]
+  (apply str (map :character document)))
+
+(defn logoot-string
+  "Returns the document as a string as represented in the logoot paper"
+  [document]
+  (str/join "\n" (map to-logoot-string document)))
 
 (defn insert
-  [])
+  [document pc]
+  (conj document pc))
 
-(def delete
-  [])
+(defn delete
+  [document position]
+  (disj document (->PositionedCharacter nil position)))
+
